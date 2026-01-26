c96a0d2223299302854f15a5e3a17992ade4c780
Nate Choung <nate.choung@northpointministries.net>
2026-01-26 12:55:30 -0500

    Fixed exit issue

--- usr/local/etc/zabbix/sh/cups_printer_status.sh
+++ usr/local/etc/zabbix/sh/cups_printer_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 printer_status=$(lpstat -p "$1" | cut -f3,4 -d ' ')
 case $printer_status in
-    "disabled since") return 1;;
-    *) return 0;;
+    "disabled since") exit 1;;
+    *) exit 0;;
 esac
