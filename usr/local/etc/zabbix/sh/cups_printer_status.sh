d761fd519551f7bb3760b0fa7ab1d4b0b12b5ece
Nate Choung <nate.choung@northpointministries.net>
2026-01-26 19:56:41 -0500

    edited cups_printer_status.sh

--- usr/local/etc/zabbix/sh/cups_printer_status.sh
+++ usr/local/etc/zabbix/sh/cups_printer_status.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-printer_status=$(lpstat -p "$1" | cut -f3,4 -d ' ')
-case $printer_status in
-    "disabled since") exit 1;;
-    *) exit 0;;
-esac
+# printer_status=$(lpstat -p "$1" | cut -f3,4 -d ' ')
+# case $printer_status in
+#     "disabled since") exit 1;;
+#     *) exit 0;;
+# esac
+
+if lpstat -p "$1" | grep -q '^.*disabled since'; then
+    echo "1"
+else
+    echo "0"
+fi
